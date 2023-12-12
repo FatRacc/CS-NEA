e663236346caeabf17f748e5f2c15a6b31584fdd
FatRacc <79345854+FatRacc@users.noreply.github.com>
2023-12-12 09:59:08 +0000

    login system added- to be worked on

--- Comp. Sci. NEA/main.py
+++ Comp. Sci. NEA/main.py
@@ -75,7 +75,7 @@ def get_font(size): # Returns Press-Start-2P in the desired size
 
 
 def play():
-    player_render()
+    main_account_screen()
 
 
 
@@ -182,6 +182,154 @@ def main_menu():
       pygame.display.update()
 
 
+#===============================================================================
+# Login system
+#===============================================================================
+def register(): #registration window
+    global register_screen
+    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    register_screen.geometry("300x250")
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+
+    Label(register_screen, text="Please enter details below", bg="blue").pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username * ")
+    username_lable.pack()
+    username_entry = Entry(register_screen, textvariable=username)
+    username_entry.pack()
+    password_lable = Label(register_screen, text="Password * ")
+    password_lable.pack()
+    password_entry = Entry(register_screen, textvariable=password, show='*')
+    password_entry.pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1, bg="blue", command = register_user).pack()
+
+def login(): #login window
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_login_entry
+    global password_login_entry
+
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
+
+def register_user(): #event on register button
+    username_info = username.get()
+    password_info = password.get()
+
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+ 
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+ 
+        else:
+            password_not_recognised()
+ 
+    else:
+        user_not_found()
+ 
+# Designing popup for login success
+ 
+def login_sucess():
+    global login_success_screen
+    login_success_screen = Toplevel(login_screen)
+    login_success_screen.title("Success")
+    login_success_screen.geometry("150x100")
+    Label(login_success_screen, text="Login Success").pack()
+    Button(login_success_screen, text="OK", command=delete_login_success).pack()
+    player_render()
+
+# Designing popup for login invalid password
+ 
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+ 
+# Designing popup for user not found
+ 
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+ 
+# Deleting popups
+ 
+def delete_login_success():
+    login_success_screen.destroy()
+ 
+ 
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+ 
+ 
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
+ 
+ 
+# Designing Main(first) window
+ 
+def main_account_screen():
+    global main_screen
+    main_screen = Tk()
+    main_screen.geometry("300x250")
+    main_screen.title("Account Login")
+    Label(text="Select Your Choice", bg="blue", width="300", height="2", font=("Calibri", 13)).pack()
+    Label(text="").pack()
+    Button(text="Login", height="2", width="30", command = login).pack()
+    Label(text="").pack()
+    Button(text="Register", height="2", width="30", command=register).pack()
+ 
+    main_screen.mainloop()
 
 
 #===============================================================================
@@ -234,10 +382,10 @@ def player_render():
 
         left, middle, right = pygame.mouse.get_pressed()
         if left:
-            time.sleep(1)
+            time.sleep(0.3)
             buildingPlacement() #allows factory building to be placed
         if right:
-            time.sleep(1)
+            time.sleep(0.3)
             beltPlacement() #allows belt to be placed
 
         
@@ -359,21 +507,27 @@ def buildingPlacement():
     else:
         buildingCoordinates.append(x_building_pos)
         buildingCoordinates.append(y_building_pos)
+        buildingFunction(x_building_pos, y_building_pos)
         print("Buildings placed at ", buildingCoordinates)
         
-        showBuilding = True
-        while showBuilding:
-            building = pygame.image.load(os.path.join("assets", "machine_assembler_tier_1.PNG"))
-            screen.blit(building, (x_building_pos, y_building_pos))
-            pygame.display.flip()
+        
+        building = pygame.image.load(os.path.join("assets", "machine_assembler_tier_1.PNG"))
+        screen.blit(building, (x_building_pos, y_building_pos))
+        pygame.display.flip()
         
 
-    
-    pygame.display.flip()
-
-
 #===============================================================================
-#Factory Building Placement
+#Factory Building function window
+#===============================================================================
+def buildingFunction(x_building_pos, y_building_pos):
+    window = tk.Tk()
+    window.title("Building Function")
+    choice = Button()
+    
+    def button_click(button_number):
+        print(f"button {button_number} clicked!")
+#===============================================================================
+#Factory Belt Placement
 #===============================================================================
 def beltPlacement():
     print("Belt Placed")#detects if mouse was clicked and displays a message if so
